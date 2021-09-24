58809bb34dad8a08dc64b1b06e796e9a538d4843
Cory Johns <johnsca@gmail.com>
2021-09-24 13:08:32 -0400

    Refactor RBAC check to not use wait_for_idle
    The behavior of `wait_for_idle` returning immediately as soon as *any*
    unit is in error, even with `raise_on_error=False` was causing some
    strangeness with the test attempting to resolve a unit before it was in
    error (somehow).

--- bundle/tests/test_bundle.py
+++ bundle/tests/test_bundle.py
@@ -4,6 +4,8 @@ import aiohttp
 async def test_build_and_deploy(ops_test, test_helpers, rbac):
     # can't use the bundle because of:
     # https://github.com/juju/python-libjuju/issues/472
+    # can't use ops_test.build_charms() because of:
+    # https://github.com/canonical/charmcraft/issues/554
     controller_charm = await ops_test.build_charm("charms/metallb-controller")
     speaker_charm = await ops_test.build_charm("charms/metallb-speaker")
     controller = await ops_test.model.deploy(
@@ -17,21 +19,33 @@ async def test_build_and_deploy(ops_test, test_helpers, rbac):
     )
 
     if rbac:
+
+        def units_in_error(expect_error):
+            def _predicate():
+                if not (controller.units and speaker.units):
+                    return False
+                controller_status = controller.units[0].workload_status
+                speaker_status = speaker.units[0].workload_status
+                if expect_error:
+                    # only error is allowed
+                    return {"error"} == {controller_status, speaker_status}
+                else:
+                    # no error is allowed
+                    return {"error"} ^ {controller_status, speaker_status}
+
+            return _predicate
+
         # confirm units go to error if RBAC rules not in place
-        await ops_test.model.wait_for_idle(raise_on_error=False, raise_on_blocked=True)
-        assert controller.units[0].workload_status == "error"
-        assert speaker.units[0].workload_status == "error"
+        await ops_test.model.block_until(units_in_error(True), timeout=5 * 60)
         # confirm adding RBAC rules enables units to resolve
         await test_helpers.apply_rbac_operator_rules()
         await controller.units[0].resolved(retry=True)
         await speaker.units[0].resolved(retry=True)
-        await ops_test.model.block_until(
-            lambda: "error"
-            not in (
-                controller.units[0].workload_status,
-                speaker.units[0].workload_status,
-            )
-        )
+        # NB: This only blocks until the units are not in error state, but they
+        # likely are in maintenance or executing instead. If we went straight to
+        # the wait_for_idle below, it would immediately fail due to the previous
+        # error states since the hooks haven't started the retry yet.
+        await ops_test.model.block_until(units_in_error(False), timeout=5 * 60)
 
     await ops_test.model.wait_for_idle(wait_for_active=True, raise_on_blocked=True)
     # test metallb
