4da206ae01aa44d2f6cbf83a3e6da49f74832ad3
Adam Dyess <adam.dyess@canonical.com>
2022-07-22 08:59:07 -0500

    enable jammy support for this charm (#21)
    * enable jammy support for this charm
    
    * support reusable workflows for inclusive naming, linting, and unit testing
    
    * upgrade Pipfile.lock to support py3.10 testing
    
    * drop python 3.7 unit testing

--- charms/metallb-controller/src/utils.py
+++ charms/metallb-controller/src/utils.py
@@ -15,69 +15,58 @@ def create_k8s_objects(namespace):
     """Create all supplementary K8s objects."""
     create_pod_security_policy_with_api(namespace=namespace)
     create_namespaced_role_with_api(
-        name='config-watcher',
+        name="config-watcher",
         namespace=namespace,
-        labels={'app': 'metallb'},
-        resources=['configmaps'],
-        verbs=['get', 'list', 'watch']
+        labels={"app": "metallb"},
+        resources=["configmaps"],
+        verbs=["get", "list", "watch"],
     )
     create_namespaced_role_binding_with_api(
-        name='config-watcher',
+        name="config-watcher",
         namespace=namespace,
-        labels={'app': 'metallb'},
-        subject_name='metallb-controller'
+        labels={"app": "metallb"},
+        subject_name="metallb-controller",
     )
 
 
 def remove_k8s_objects(namespace):
     """Remove all supplementary K8s objects."""
-    delete_pod_security_policy_with_api(name='controller')
-    delete_namespaced_role_binding_with_api(
-        name='config-watcher',
-        namespace=namespace
-    )
-    delete_namespaced_role_with_api(
-        name='config-watcher',
-        namespace=namespace
-    )
+    delete_pod_security_policy_with_api(name="controller")
+    delete_namespaced_role_binding_with_api(name="config-watcher", namespace=namespace)
+    delete_namespaced_role_with_api(name="config-watcher", namespace=namespace)
 
 
 def create_pod_security_policy_with_api(namespace):
     """Create pod security policy."""
     # Using the API because of LP:1886694
-    logging.info('Creating pod security policy with K8s API')
+    logging.info("Creating pod security policy with K8s API")
     _load_kube_config()
 
     metadata = client.V1ObjectMeta(
-        namespace=namespace,
-        name='controller',
-        labels={'app': 'metallb'}
+        namespace=namespace, name="controller", labels={"app": "metallb"}
     )
     policy_spec = client.PolicyV1beta1PodSecurityPolicySpec(
         allow_privilege_escalation=False,
         default_allow_privilege_escalation=False,
         fs_group=client.PolicyV1beta1FSGroupStrategyOptions(
-            ranges=[client.PolicyV1beta1IDRange(max=65535, min=1)],
-            rule='MustRunAs'
+            ranges=[client.PolicyV1beta1IDRange(max=65535, min=1)], rule="MustRunAs"
         ),
         host_ipc=False,
         host_network=False,
         host_pid=False,
         privileged=False,
         read_only_root_filesystem=True,
-        required_drop_capabilities=['ALL'],
+        required_drop_capabilities=["ALL"],
         run_as_user=client.PolicyV1beta1RunAsUserStrategyOptions(
-            ranges=[client.PolicyV1beta1IDRange(max=65535, min=1)],
-            rule='MustRunAs'
+            ranges=[client.PolicyV1beta1IDRange(max=65535, min=1)], rule="MustRunAs"
         ),
         se_linux=client.PolicyV1beta1SELinuxStrategyOptions(
-            rule='RunAsAny',
+            rule="RunAsAny",
         ),
         supplemental_groups=client.PolicyV1beta1SupplementalGroupsStrategyOptions(
-            ranges=[client.PolicyV1beta1IDRange(max=65535, min=1)],
-            rule='MustRunAs'
+            ranges=[client.PolicyV1beta1IDRange(max=65535, min=1)], rule="MustRunAs"
         ),
-        volumes=['configMap', 'secret', 'emptyDir'],
+        volumes=["configMap", "secret", "emptyDir"],
     )
 
     body = client.PolicyV1beta1PodSecurityPolicy(metadata=metadata, spec=policy_spec)
@@ -106,28 +95,29 @@ def delete_pod_security_policy_with_api(name):
         try:
             api_instance.delete_pod_security_policy(name=name, body=body, pretty=True)
         except ApiException:
-            logging.exception("Exception when calling PolicyV1beta1Api"
-                              "->delete_pod_security_policy.")
+            logging.exception(
+                "Exception when calling PolicyV1beta1Api"
+                "->delete_pod_security_policy."
+            )
 
 
-def create_namespaced_role_with_api(name, namespace, labels, resources, verbs,
-                                    api_groups=['']):
+def create_namespaced_role_with_api(
+    name, namespace, labels, resources, verbs, api_groups=[""]
+):
     """Create namespaced role."""
     # Using API because of bug https://bugs.launchpad.net/juju/+bug/1896076
-    logging.info('Creating namespaced role with K8s API')
+    logging.info("Creating namespaced role with K8s API")
     _load_kube_config()
 
     body = client.V1Role(
-        metadata=client.V1ObjectMeta(
-            name=name,
-            namespace=namespace,
-            labels=labels
-        ),
-        rules=[client.V1PolicyRule(
-            api_groups=api_groups,
-            resources=resources,
-            verbs=verbs,
-        )]
+        metadata=client.V1ObjectMeta(name=name, namespace=namespace, labels=labels),
+        rules=[
+            client.V1PolicyRule(
+                api_groups=api_groups,
+                resources=resources,
+                verbs=verbs,
+            )
+        ],
     )
     with client.ApiClient() as api_client:
         api_instance = client.RbacAuthorizationV1Api(api_client)
@@ -144,7 +134,7 @@ def create_namespaced_role_with_api(name, namespace, labels, resources, verbs,
 
 def delete_namespaced_role_with_api(name, namespace):
     """Delete namespaced role."""
-    logging.info('Deleting namespaced role with K8s API')
+    logging.info("Deleting namespaced role with K8s API")
     _load_kube_config()
 
     body = client.V1DeleteOptions()
@@ -152,40 +142,33 @@ def delete_namespaced_role_with_api(name, namespace):
         api_instance = client.RbacAuthorizationV1Api(api_client)
         try:
             api_instance.delete_namespaced_role(
-                name=name,
-                namespace=namespace,
-                body=body,
-                pretty=True
+                name=name, namespace=namespace, body=body, pretty=True
             )
         except ApiException:
-            logging.exception("Exception when calling RbacAuthorizationV1Api"
-                              "->delete_namespaced_role.")
+            logging.exception(
+                "Exception when calling RbacAuthorizationV1Api"
+                "->delete_namespaced_role."
+            )
 
 
-def create_namespaced_role_binding_with_api(name, namespace, labels, subject_name,
-                                            subject_kind='ServiceAccount'):
+def create_namespaced_role_binding_with_api(
+    name, namespace, labels, subject_name, subject_kind="ServiceAccount"
+):
     """Bind namespaced role to subject."""
     # Using API because of bug https://bugs.launchpad.net/juju/+bug/1896076
-    logging.info('Creating role binding with K8s API')
+    logging.info("Creating role binding with K8s API")
     _load_kube_config()
 
     body = client.V1RoleBinding(
-        metadata=client.V1ObjectMeta(
-            name=name,
-            namespace=namespace,
-            labels=labels
-        ),
+        metadata=client.V1ObjectMeta(name=name, namespace=namespace, labels=labels),
         role_ref=client.V1RoleRef(
-            api_group='rbac.authorization.k8s.io',
-            kind='Role',
+            api_group="rbac.authorization.k8s.io",
+            kind="Role",
             name=name,
         ),
         subjects=[
-            client.V1Subject(
-                kind=subject_kind,
-                name=subject_name
-            ),
-        ]
+            client.V1Subject(kind=subject_kind, name=subject_name),
+        ],
     )
     with client.ApiClient() as api_client:
         api_instance = client.RbacAuthorizationV1Api(api_client)
@@ -202,7 +185,7 @@ def create_namespaced_role_binding_with_api(name, namespace, labels, subject_nam
 
 def delete_namespaced_role_binding_with_api(name, namespace):
     """Delete namespaced role binding with K8s API."""
-    logging.info('Deleting namespaced role binding with API')
+    logging.info("Deleting namespaced role binding with API")
     _load_kube_config()
 
     body = client.V1DeleteOptions()
@@ -210,25 +193,25 @@ def delete_namespaced_role_binding_with_api(name, namespace):
         api_instance = client.RbacAuthorizationV1Api(api_client)
         try:
             api_instance.delete_namespaced_role_binding(
-                name=name,
-                namespace=namespace,
-                body=body,
-                pretty=True
+                name=name, namespace=namespace, body=body, pretty=True
             )
         except ApiException:
-            logging.exception("Exception when calling RbacAuthorizationV1Api"
-                              "->delete_namespaced_role_binding.")
+            logging.exception(
+                "Exception when calling RbacAuthorizationV1Api"
+                "->delete_namespaced_role_binding."
+            )
 
 
 def _random_secret(length):
     letters = string.ascii_letters
-    result_str = ''.join(random.SystemRandom().choice(letters) for i in range(length))
+    result_str = "".join(random.SystemRandom().choice(letters) for i in range(length))
     return result_str
 
 
 def _load_kube_config():
     # TODO: Remove this workaround when bug LP:1892255 is fixed
     from pathlib import Path
+
     os.environ.update(
         dict(
             e.split("=")
